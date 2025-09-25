aed1d2586e644ab378ecbeb509615c66aa51c116
smalipedd1 <shasi.malipeddi@gmail.com>
2025-09-25 11:51:47 -0400

    Add Mongoose connection options for Render stability

--- index.js
+++ index.js
@@ -11,9 +11,14 @@ app.get('/', (req, res) => {
   res.send('Cricket Academy API is running');
 });
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
   .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
+.catch(err => {
+  console.error('MongoDB connection error:', err);
+});
 
 const PORT = process.env.PORT || 5000;
 
