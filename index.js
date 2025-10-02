36c84b98c96452932bcd1ef04877615c37384f33
smalipedd1 <shasi.malipeddi@gmail.com>
2025-10-02 10:25:47 -0400

    Remove deprecated useNewUrlParser from MongoDB connection

--- index.js
+++ index.js
@@ -15,10 +15,7 @@ app.get('/', (req, res) => {
 mongoose.set('bufferCommands', false);
 
 // Connect to MongoDB with recommended options
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB connected'))
 .catch((err) => console.error('MongoDB connection error:', err));
 
