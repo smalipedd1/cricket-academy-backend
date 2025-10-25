122b021ba47465781a8ac1458b90962c9371b459
smalipedd1 <shasi.malipeddi@gmail.com>
2025-10-25 19:36:06 -0400

    get session by coach

--- routes/sessions.js
+++ routes/sessions.js
@@ -11,9 +11,9 @@ router.get('/', verifyRole('coach','admin'), async (req, res) => {
     const { focusArea, startDate, endDate } = req.query;
     const filter = {};
 
-	if (req.user.role === 'coach') {
-  	filter.coach = req.user._id;
-	}
+    if (req.user.role === 'coach') {
+      filter.coach = req.user._id;
+    }
 
     if (focusArea) filter.focusArea = focusArea;
     if (startDate && endDate) {
