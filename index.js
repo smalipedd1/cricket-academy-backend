9d71c1e2880321946b69519dbcdfa92c98e2d694
smalipedd1 <shasi.malipeddi@gmail.com>
2025-10-29 10:05:35 -0400

    added player routes for dashboard

--- index.js
+++ index.js
@@ -45,6 +45,3 @@ const coachRoutes = require('./routes/coach');
 app.use('/api/coaches', coachRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-const playerRoutes = require('./routes/player');
-app.use('/api/player', playerRoutes);
